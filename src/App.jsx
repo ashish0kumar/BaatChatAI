ceffa69bbd0f23fcbb34ddd6073459da2cd983eb
Ashish Kumar <ashishkumar901336@gmail.com>
2024-07-01 14:32:23 +0530

    main page ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Sidebar from "./Components/Sidebar/Sidebar"
+import Main from "./Components/Main/Main"
 
 function App() {
   return (
     <>
       <Sidebar/>
+      <Main/>
     </>
   )
 }
